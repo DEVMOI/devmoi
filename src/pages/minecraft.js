59175f0632ef47d363cc9776bcef2eb8eb304e07
Warren Gates <warrenckhan@gmail.com>
2020-03-25 19:57:58 -0400

    added additional  styling b4 merge into dev

--- src/pages/minecraft.js
+++ src/pages/minecraft.js
@@ -21,23 +21,22 @@ export default () => {
         </h4>
       </div>
       <div className="p-5 d-flex flex-column justify-content-center ">
-        <p className="lead pt-3">
+        <p
+          className="lead pt-3 text-center"
+          onClick={() => {
+            copyToClipboard('moia.ramshard.net');
+          }}>
           Server IP:{` `}
-          <span
-            title="Click to Copy"
-            className="minecraft-ip text-monospace"
-            onClick={() => {
-              copyToClipboard('moia.ramshard.net');
-            }}>
+          <span title="Click to Copy" className="minecraft-ip text-monospace">
             <u>moia.ramshard.net</u>
           </span>
         </p>
-        <p>
+        <p className="w-75 mx-auto">
           We host a Minecraft Server for our Community, and open to the public
           as well as all ages, so we ask that you follow a few rules to keep
           things as chill and as fun as possible.
         </p>
-        <div className="d-flex flex-column align-content-center pt-3">
+        <div className="d-flex flex-column align-content-center mx-auto pt-3">
           <p className="lead mb-0">Server Rules:</p>
           <ol>
             <li>Be Chill</li>
@@ -46,9 +45,9 @@ export default () => {
             <li>Respect the Mods</li>
           </ol>
         </div>
-        <div className="pt-4 w-100">
+        <div className="pt-4 text-center w-100">
           <a
-            className="btn w-100 btn-outline-dark"
+            className="btn btn-outline-dark"
             href="http://moia.ramshard.net:8123/index.html?worldname=Moia&mapname=flat&zoom=5&x=-25&y=64&z=-526"
             target="_blank"
             rel="noopener noreferrer">
