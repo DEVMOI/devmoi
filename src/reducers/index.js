a0a5f21ae1e25f23676bfeb319b7c45de2a1b19b
Moikapy <warrenckhan@gmail.com>
2020-10-13 12:16:03 -0400

    remove reducers imported

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,6 @@ import { combineReducers } from 'redux';
 
 import authReducer from './authReducer';
 import errorReducer from './errorReducer';
-import usermanagementReducer from './usermanagementReducer';
-// import mediaReducer from './mediaReducer';
-import pageReducer from './pageReducer';
 
 const rootReducer = combineReducers({
   authReducer,
