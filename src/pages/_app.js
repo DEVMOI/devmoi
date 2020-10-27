95192b30d41bf9e25bb765f123e2ddf499faa276
Moikapy <warrenckhan@gmail.com>
2020-10-27 17:25:17 -0400

    $Dev/@Moikapy Updated Package to NextJS10 to utilize the metrics funct

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -9,49 +9,47 @@ import Head from 'next/head';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { Layout } from '../components';
+// pages/_app.js
+export function reportWebVitals(metric) {
+  console.log(metric);
+}
+// export async function getInitialProps({ Component, ctx }) {
+//   let pageProps = {};
 
-class MyApp extends App {
-  static async getInitialProps({ Component, ctx }) {
-    let pageProps = {};
+//   if (Component.getInitialProps) {
+//     pageProps = await Component.getInitialProps(ctx);
+//   }
 
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
-    }
+//   return { pageProps };
+// }
+function MyApp({ Component, pageProps }) {
+  return (
+    <div className="h-100">
+      <Head>
+        <meta
+          name="viewport"
+          content="initial-scale=1.0, width=device-width"
+          key="viewport"
+        />
 
-    return { pageProps };
-  }
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <title>DevMoi</title>
+      </Head>
 
-  render() {
-    const { Component, pageProps } = this.props;
-
-    return (
       <div className="h-100">
-        <Head>
-          <meta
-            name="viewport"
-            content="initial-scale=1.0, width=device-width"
-            key="viewport"
-          />
-
-          <link rel="manifest" href="/manifest.json" />
-          <link rel="shortcut icon" href="/favicon.ico" />
-          <title>DevMoi</title>
-        </Head>
-
-        <div className="h-100">
-          <Provider session={pageProps.session} store={store}>
-            {/* <ThemeProvider theme={theme}> */}
-              <Layout
-                isFluid={true}
-                classes="h-100 border border-top-0 border-dark">
-                <Component {...pageProps} />
-              </Layout>
-            {/* </ThemeProvider> */}
-          </Provider>
-        </div>
+        <Provider session={pageProps.session} store={store}>
+          {/* <ThemeProvider theme={theme}> */}
+          <Layout
+            isFluid={true}
+            classes="h-100 border border-top-0 border-dark">
+            <Component {...pageProps} />
+          </Layout>
+          {/* </ThemeProvider> */}
+        </Provider>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default MyApp;
