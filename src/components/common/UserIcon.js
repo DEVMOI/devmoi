888a82369c64a39697ebc9f8110f2bf058804aca
Moikapy <warrenckhan@gmail.com>
2020-10-28 00:38:52 -0400

    create blockie component since Blockies is acting weird

--- src/components/common/UserIcon.js
+++ src/components/common/UserIcon.js
@@ -1,36 +1,50 @@
 import { useEffect } from 'react';
 import { connect } from 'react-redux';
-const blockies = require('blockies');
-
+import Blockies from './blockie';
+import { Blockie } from 'rimble-ui';
 function UserIcon(props) {
-  useEffect(() => {
-    let addrIcon = document.body.querySelector('#' + props.id);
-    if (addrIcon !== null && addrIcon.childNodes.length === 0) {
-      let icon = blockies({
-        seed: `${props.userAddress}`,
-        size: 8,
-        scale: props.scale || 3,
-      });
-      icon.classList.add('rounded-circle');
-      icon.classList.add('border');
-      icon.classList.add('border-dark');
-      icon.title = props.userAddress;
-      addrIcon.appendChild(icon);
-    }
-  }, []);
+  // useEffect(() => {
+  //   let addrIcon = document.body.querySelector('#' + props.id);
+  //   console.log(addrIcon);
+
+  //   let icon = blockies.create({
+  //     // All options are optional
+  //     seed: `${props.address}`, // seed used to generate icon data, default: random
+  //     // color: '#dfe', // to manually specify the icon color, default: random
+  //     // bgcolor: '#aaa', // choose a different background color, default: random
+  //     size: 8, // width/height of the icon in blocks, default: 8
+  //     scale: props.scale || 3, // width/height of each block in pixels, default: 4
+  //     // spotcolor: '#000', // each pixel has a 13% chance of being of a third color,
+  //     // default: random. Set to -1 to disable it. These "spots" create structures
+  //     // that look like eyes, mouths and noses.
+  //   });
+  //   icon.classList.add('rounded-circle');
+  //   icon.classList.add('border');
+  //   icon.classList.add('border-dark');
+  //   icon.title = props.address;
+  //   addrIcon.appendChild(icon);
+  //   console.log(icon);
+  // }, [props.address]);
 
   return (
-    <div
-      id={props.id}
-      className={
-        props.userIconStyle !== undefined
-          ? `mt-1 ${props.userIconStyle}`
-          : 'mt-1'
-      }
+    <Blockies
+      opts={{
+        seed: '0xa8D145Dd3003817dA1DC83F838Ee5088B65Acf2e' || props.address,
+        size: 8,
+        scale: props.scale || 3,
+      }}
     />
   );
+  // return (
+  //   <div
+  //     id={props.id}
+  //     className={`w-100 ${
+  //       props.userIconStyle !== undefined ? `${props.userIconStyle}` : null
+  //     }`}
+  //   />
+  // );
 }
 const mapStateToProps = (state) => ({
-  userAddress: state.session.address,
+  address: state.session.address,
 });
-export default connect(mapStateToProps, {})(UserIcon);
+export default connect(mapStateToProps)(UserIcon);
---
+++ src/components/common/blockie.js
@@ -0,0 +1,130 @@
+import React, { Component, useEffect, useRef, useState } from 'react';
+
+// The random number is a js implementation of the Xorshift PRNG
+const randseed = new Array(4); // Xorshift: [x, y, z, w] 32 bit values
+
+function seedrand(seed) {
+  randseed.fill(0);
+
+  for (let i = 0; i < seed.length; i++) {
+    (randseed[i % 4] =
+      (randseed[i % 4] << 5) - randseed[i % 4] + seed.charCodeAt(i));
+  }
+  console.log(randseed);
+}
+
+function rand() {
+  console.log(randseed)
+  // based on Java's String.hashCode(), expanded to 4 32bit values
+  const t = randseed[0] ^ (randseed[0] << 11);
+
+  randseed[0] = randseed[1];
+  randseed[1] = randseed[2];
+  randseed[2] = randseed[3];
+  randseed[3] = randseed[3] ^ (randseed[3] >> 19) ^ t ^ (t >> 8);
+
+  return (randseed[3] >>> 0) / ((1 << 31) >>> 0);
+}
+
+function createColor() {
+  //saturation is the whole color spectrum
+  const h = Math.floor(rand() * 360);
+  //saturation goes from 40 to 100, it avoids greyish colors
+  const s = rand() * 60 + 40 + '%';
+  //lightness can be anything from 0 to 100, but probabilities are a bell curve around 50%
+  const l = (rand() + rand() + rand() + rand()) * 25 + '%';
+
+  return 'hsl(' + h + ',' + s + ',' + l + ')';
+}
+
+function createImageData(size) {
+  const width = size; // Only support square icons for now
+  const height = size;
+
+  const dataWidth = Math.ceil(width / 2);
+  const mirrorWidth = width - dataWidth;
+
+  const data = [];
+  for (let y = 0; y < height; y++) {
+    let row = [];
+    for (let x = 0; x < dataWidth; x++) {
+      // this makes foreground and background color to have a 43% (1/2.3) probability
+      // spot color has 13% chance
+      row[x] = Math.floor(rand() * 2.3);
+    }
+    const r = row.slice(0, mirrorWidth);
+    r.reverse();
+    row = row.concat(r);
+
+    for (let i = 0; i < row.length; i++) {
+      data.push(row[i]);
+    }
+  }
+
+  return data;
+}
+
+function buildOpts(opts) {
+  const newOpts = {};
+
+  newOpts.seed =
+    opts.seed || Math.floor(Math.random() * Math.pow(10, 16)).toString(16);
+
+  seedrand(newOpts.seed);
+
+  newOpts.size = opts.size || 8;
+  newOpts.scale = opts.scale || 4;
+  newOpts.color = opts.color || createColor();
+  newOpts.bgcolor = opts.bgcolor || createColor();
+  newOpts.spotcolor = opts.spotcolor || createColor();
+
+  return newOpts;
+}
+
+export function RenderIcon({ opts }) {
+  let blockieIcon = useRef(null);
+  const [blockie, setBlockie] = useState(null);
+  useEffect(() => {
+    opts = buildOpts(opts || {});
+    const imageData = createImageData(opts.size);
+    const width = Math.sqrt(imageData.length);
+    blockieIcon.current.width = blockieIcon.current.height =
+      opts.size * opts.scale;
+
+    const cc = blockieIcon.current.getContext('2d');
+    cc.fillStyle = opts.bgcolor;
+    cc.fillRect(0, 0, blockieIcon.current.width, blockieIcon.current.height);
+    cc.fillStyle = opts.color;
+
+    for (let i = 0; i < imageData.length; i++) {
+      // if data is 0, leave the background
+      if (imageData[i]) {
+        const row = Math.floor(i / width);
+        const col = i % width;
+
+        // if data is 2, choose spot color, if 1 choose foreground
+        cc.fillStyle = imageData[i] == 1 ? opts.color : opts.spotcolor;
+
+        cc.fillRect(col * opts.scale, row * opts.scale, opts.scale, opts.scale);
+      }
+    }
+    console.log(blockieIcon);
+    setBlockie(blockieIcon);
+  }, []);
+  useEffect(() => {
+    console.log(blockie == blockieIcon);
+  }, [blockie]);
+  // console.log(blockie.current)
+  // let current = blockie !== null ? blockie.current : null;
+  // return current !== null || current !== undefined ? (
+  //   current
+  // ) :
+
+  return <canvas ref={blockieIcon} />;
+}
+
+function Blockies({ opts }) {
+  return <RenderIcon opts={opts} />;
+}
+
+export default Blockies;
