7cce049d3884a74d063ac8ca36975ad0ad7c6845
Moikapy <warrenckhan@gmail.com>
2020-11-03 23:31:24 -0500

    improved Team Member Card

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,18 +1,23 @@
 import { UserIcon } from '@/components/common';
+import { useEffect, useState, useRef } from 'react';
 import RSSFeed from '@/components/RSSFeed';
+import { Slider, Input } from 'rimble-ui';
 
-const handleConversion = async (addr) => {
+const donateEth = async (addr, value = 1) => {
   try {
     await window.ethereum.enable();
 
     const response = await fetch(
-      `https://api.0x.org/swap/v1/quote?buyToken=DAI&sellToken=ETH&buyAmount=1000000000000000000`
+      `https://api.0x.org/swap/v1/quote?buyToken=DAI&sellToken=ETH&buyAmount=${web3.utils.toWei(
+        `${value}`,
+        'ether'
+      )}`
     );
     if (response.ok) {
-      let data = await response.json();
-      data.to = await addr;
-
       if (window.ethereum) {
+        let data = await response.json();
+        data.to = await addr;
+        data.from = ethereum.selectedAdress;
         web3.eth.sendTransaction(data);
       }
     } else {
@@ -25,14 +30,46 @@ const handleConversion = async (addr) => {
 };
 
 function TeamCard(props) {
+  const [donationValue, setDonationValue] = useState(1);
   return (
-    <div title={props.seed} className="border border-dark p-3">
+    <div title={props.seed} className="team-card border border-dark p-3">
+      <style jsx>
+        {`
+          .team-card{
+            200px;
+          }
+          `}
+      </style>
       <UserIcon seed={props.seed} />
       <p>{props.role}</p>
+      <div className="d-flex flex-column">
+        <div className="d-flex flex-row align-items-center">
+          <Input
+            type="number"
+            required={true}
+            placeholder="e.g. 123"
+            value={donationValue}
+            onChange={(e) =>
+              e.target.value >= 1 ? setDonationValue(e.target.value) : null
+            }
+          />
+          <span className="font-weight-bold text-uppercase mx-3 ml-4">DAI</span>
+        </div>
+        <Slider
+          width="100%"
+          min={'1'}
+          max={'100'}
+          step={'1'}
+          defaultValue={donationValue}
+          onChange={(e) =>
+            e.target.value >= 1 ? setDonationValue(e.target.value) : null
+          }
+        />
+      </div>
       <button
         className="w-100 btn m-0 p-0 border border-top"
         onClick={() => {
-          handleConversion(props.seed);
+          donateEth(props.seed, donationValue);
         }}>
         Donate
       </button>
