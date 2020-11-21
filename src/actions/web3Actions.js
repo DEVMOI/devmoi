4ace1298fbf8338087f337a97d9269e8e582e97b
Moikapy <warrenckhan@gmail.com>
2020-11-20 21:46:43 -0500

    .

--- src/actions/web3Actions.js
+++ src/actions/web3Actions.js
@@ -36,7 +36,7 @@ const _isMetaMask = async () => {
 export const initWeb3 = () => async (dispatch) => {
   const api = process.env.INFURA_API;
   try {
-    if (window.ethereum) {
+    if (window.ethereum!==undefined) {
       window.web3 = new Web3(window.ethereum);
 
       return true;
