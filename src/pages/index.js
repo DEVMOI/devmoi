d82cddfc2adfac07c899d9e71b7d29194b887669
Moikapy <warrenckhan@gmail.com>
2020-11-24 21:57:36 -0500

    fixed home style and added button to point to connect button

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ const UserInfo = dynamic(() => import('@/components/UserInfo'), { ssr: false });
 
 
 const Home = ({ address }) => (
-  <div className="home">
+  <div className="home container">
     <style jsx>
       {`
         .address {
@@ -34,7 +34,10 @@ const Home = ({ address }) => (
       <p className={`text-capitalize fnt-size-16`}>
         Currently an Experimental Site utilizing various api to bring what web3
         has to offer to you.
-      </p>
+      </p> 
+      <p>To have a full experience connect your wallet <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-up-right-circle-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.879 10.828a.5.5 0 1 1-.707-.707l4.096-4.096H6.5a.5.5 0 0 1 0-1h3.975a.5.5 0 0 1 .5.5V9.5a.5.5 0 0 1-1 0V6.732l-4.096 4.096z"/>
+</svg></p>
     </div>
   </div>
 );
